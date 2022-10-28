40130eb2b237ed05b1b0e0bc3e3640828a9d5172
dianaostik <dianastud12@gmail.com>
2022-10-28 13:54:15 +0300

    5com

---
+++ js/main.js
@@ -0,0 +1,75 @@
+
+// 1 usduotis
+console.log("----------- 1 uzduotis -------------");
+
+let sk1 = 5;
+let sk2 = 6;
+
+sum(sk1, sk2);
+
+function sum(a, b) {
+    console.log(a * b);   
+}
+
+
+
+//2 uzduotis
+console.log("----------- 2 uzduotis -------------");
+
+let vardas = "Petras";
+
+sayHiTo(vardas);
+
+function sayHiTo(name) {
+    console.log("Labas " + name);   
+}
+
+
+
+
+//3 uzduotis
+console.log("----------- 3 uzduotis -------------");
+
+let text = "Labukas";
+
+kiek(text);
+
+function kiek(textTwo) {
+    console.log(textTwo.length);
+}
+
+
+
+
+//4 uzduotis
+console.log("----------- 4 uzduotis -------------");
+
+let vard = "jonas";
+let pavarde = "petrauskas";
+
+rezult(vard,pavarde);
+
+function rezult(c, d) {
+    console.log(c[0].toUpperCase() + d[0].toUpperCase());
+}
+
+
+
+//5 uzduotis
+console.log("----------- 5 uzduotis -------------");
+
+let x = 20;
+
+dokumentas(x);
+
+function dokumentas(y) {
+    document.getElementById("numberPlace").innerText = y;
+}
+
+
+
+
+
+//6 uzduotis
+console.log("----------- 6 uzduotis -------------");
+
